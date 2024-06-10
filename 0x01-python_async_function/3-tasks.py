f094cd38c0530cf5caec3af5ca76fd57d96a20a7
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-06-10 21:25:11 +0100

    task 3 created

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""function that take max_delay and return asynio.task"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay) -> asyncio.Task:
+    """create an instance of wait_random and ruturn the task"""
+    return asyncio.create_task(wait_random(max_delay))
