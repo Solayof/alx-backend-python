1ad77311c8d3b0e8fe09dc51b283d60598e36acc
solomonayofemi@gmail.com <Moses Solomon Ayofemi>
2024-06-10 10:00:40 +0100

    task 0 file created

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""random waiting module"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """wait for a random number"""
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
